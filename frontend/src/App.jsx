17323ef9089030844bb4c91f9fc6309a8af724de
adams <adamskiptalam0@gmail.com>
2025-05-23 15:56:09 +0300

    Add routing to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { Box } from "@chakra-ui/react";
-import { Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import { useColorModeValue } from "@chakra-ui/react";
 
 function App() {
 	return (
 		<>
-			<Box minH={"100vh"}>
-				{/* <Navbar /> */}
+			<Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+				<Navbar />
 				<Routes>
 					<Route path="/" element={<Homepage />} />
 					<Route path="/create" element={<CreatePage />} />
