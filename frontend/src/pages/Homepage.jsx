d1345a6214db319b164fb329e88c55cbcbb81ced
adams <adamskiptalam0@gmail.com>
2025-05-24 15:15:43 +0300

    Display products on homepage

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,8 +1,15 @@
-import React from "react";
-import { Container, Text, VStack } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { Container, SimpleGrid, Text, VStack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { useProductStore } from "../store/product.js";
+import ProductCard from "../components/ProductCard.jsx";
 
 const Homepage = () => {
+	const { fetchProducts, products } = useProductStore();
+	useEffect(() => {
+		fetchProducts();
+	}, [fetchProducts]);
+	console.log("products", products);
 	return (
 		<Container maxW="container.xl" py={12}>
 			<VStack spacing={8}>
@@ -15,21 +22,36 @@ const Homepage = () => {
 					Current Products
 				</Text>
 
-				<Text
-					fontSize={"xl"}
-					fontWeight={"bold"}
-					color={"gray.500"}
-					textAlign={"center"}>
-					No products found{" "}
-					<Link to={"/create"}>
-						<Text
-							as={"span"}
-							color={"blue.500"}
-							_hover={{ textDecoration: "underline" }}>
-							Create a product
-						</Text>
-					</Link>
-				</Text>
+				<SimpleGrid
+					columns={{
+						base: 1,
+						md: 2,
+						lg: 3,
+					}}
+					spacing={10}
+					w={"full"}>
+					{products.map((product) => (
+						<ProductCard key={product._id} product={product} />
+					))}
+				</SimpleGrid>
+
+				{products.length === 0 && (
+					<Text
+						fontSize={"xl"}
+						fontWeight={"bold"}
+						color={"gray.500"}
+						textAlign={"center"}>
+						No products found 😢{" "}
+						<Link to={"/create"}>
+							<Text
+								as={"span"}
+								color={"blue.500"}
+								_hover={{ textDecoration: "underline" }}>
+								Create a product
+							</Text>
+						</Link>
+					</Text>
+				)}
 			</VStack>
 		</Container>
 	);
--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -18,4 +18,9 @@ export const useProductStore = create((set) => ({
 
 		return { success: true, message: "Product created successfully" };
 	},
+	fetchProducts: async () => {
+		const res = await fetch("/api/products");
+		const data = await res.json();
+		set({ products: data.data });
+	},
 }));
