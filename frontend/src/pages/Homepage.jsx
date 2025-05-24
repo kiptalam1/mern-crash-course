f48361d5e3b18db911b47243b894f3e12e350fb7
adams <adamskiptalam0@gmail.com>
2025-05-24 13:46:07 +0300

    create the starting homepage

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { Container, Text, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
-	return <div>Homepage</div>;
+	return (
+		<Container maxW="container.xl" py={12}>
+			<VStack spacing={8}>
+				<Text
+					fontSize={"30"}
+					fontWeight={"bold"}
+					bgGradient={"linear(to-r, cyan.400, blue.500)"}
+					bgClip={"text"}
+					textAlign={"center"}>
+					Current Products
+				</Text>
+
+				<Text
+					fontSize={"xl"}
+					fontWeight={"bold"}
+					color={"gray.500"}
+					textAlign={"center"}>
+					No products found{" "}
+					<Link to={"/create"}>
+						<Text
+							as={"span"}
+							color={"blue.500"}
+							_hover={{ textDecoration: "underline" }}>
+							Create a product
+						</Text>
+					</Link>
+				</Text>
+			</VStack>
+		</Container>
+	);
 };
 
 export default Homepage;
